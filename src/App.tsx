fc2e2614e9559cd3c86e0e0965e8d5aa7ba9bc20
ipieropan <igorpieropan@gmail.com>
2024-03-25 11:17:20 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Title } from "./appStyled";
+
+function App() {
+	return (
+		<>
+      <Title>This is the home page</Title>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/movieCard/movieCard.tsx
@@ -0,0 +1,11 @@
+function MovieCard() {
+    return (
+        <>
+            <div>
+                
+            </div>
+        </>
+    )
+}
+
+export default MovieCard;
